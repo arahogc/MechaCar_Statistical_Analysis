ed9a32af6accdfab937e2205371c5fd7edc7633d
arahogc <74072742+arahogc@users.noreply.github.com>
2021-03-06 15:20:46 -0800

    Did t-tests on PSI values of lots

--- MechaCharChallenge.RScript.R
+++ MechaCharChallenge.RScript.R
@@ -10,4 +10,12 @@ SuspCoil_Table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFac
 
 Total_Summary <- SuspCoil_Table %>% summarize(Mean=mean(PSI), Medain=median(PSI), Variance=(var(PSI)), SD=(sd(PSI, na.rm = FALSE)), .groups = 'keep') #create summary table for PSI column
 
-Lot_Summary <- SuspCoil_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Medain=median(PSI), Variance=(var(PSI)), SD=(sd(PSI, na.rm = FALSE)), .groups = 'keep') #create summary table for each manufactoring lot
+Lot_Summary <- SuspCoil_Table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Medain=median(PSI), Variance=(var(PSI)), SD=(sd(PSI, na.rm = FALSE)), .groups = 'keep') #create summary table for each manufacturing lot
+
+#Deliverable 3 
+
+t.test((SuspCoil_Table$PSI),mu=1500) #single T test for PSI
+
+t.test(subset(SuspCoil_Table, Manufacturing_Lot == 'Lot1')$PSI, mu=1500) #single T test for PSI of Lot1
+t.test(subset(SuspCoil_Table, Manufacturing_Lot == 'Lot2')$PSI, mu=1500) #single T test for PSI of Lot2
+t.test(subset(SuspCoil_Table, Manufacturing_Lot == 'Lot3')$PSI, mu=1500) #single T test for PSI of Lot1
